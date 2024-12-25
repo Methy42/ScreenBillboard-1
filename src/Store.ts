548264fff5fae2482ced511dad0028461426455c
tanghuayu <tanghuayu@archeros.com>
2024-12-25 09:46:55 +0800

    Refactor asset paths in Store.ts to remove leading slashes for consistency

--- src/Store.ts
+++ src/Store.ts
@@ -2,19 +2,19 @@ export class Store {
     static assetsMap = {
         ScreenHeaderBackground: {
             type: 'image',
-            src: '/images/header_title.png'
+            src: 'images/header_title.png'
         },
         ScreenPanelBoxBackground: {
             type: 'image',
-            src: '/images/content_box_bg.png',
+            src: 'images/content_box_bg.png',
         },
         ScreenPanelTitleBackground: {
             type: 'image',
-            src: '/images/content_title_bg.png'
+            src: 'images/content_title_bg.png'
         },
         ScreenBackground: {
             type: 'image',
-            src: '/images/bg.jpg'
+            src: 'images/bg.jpg'
         }
     };
 }
